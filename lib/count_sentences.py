445e14535492f291e703cf9cc41023eda5e91938
Theodore Smith <tjs7321@gmail.com>
2023-08-20 07:50:11 -0400

    t

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+
+  def __init__(self, value=''):
+    self.value = value
+
+  def get_value(self):
+    return self._value
+  
+  def set_value(self, value):
+    if type(value) == str:
+      self._value = value
+    else: print("The value must be a string.")
+
+  def is_sentence(self):
+    if (list(self.value))[-1] == '.':
+      return True
+    return False
+
+  def is_question(self):
+    if (list(self.value))[-1] == '?':
+      return True
+    return False
+
+  def is_exclamation(self):
+    if (list(self.value))[-1] == '!':
+      return True
+    return False
+
+  def count_sentences(self):
+    spread = re.split(r"[.?!]\s*", self.value)
+    a = spread.count('')
+    b = len(spread)
+    return b-a
+  
+  value = property(get_value, set_value)
